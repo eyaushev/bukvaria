7e1f954b79eb0d4ebf25b0252a22fe02066fc621
Яушев Эдуард Маратович <yaushev_em@magnit.ru>
2026-05-04 18:14:58 +0300

    new build

--- application.96810.js
+++ application.96810.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.1267f.json';
+          this.settingsPath = 'src/settings.623fd.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
